b9f79205011703b6a4a90e45c8958cc13c0b9fde
hellfirebrewery <hellfirebrewery@gmail.com>
2025-07-07 20:49:26 +0000

    commit 3a

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
-
+    clock = pygame.time.Clock()
+    dt = 0
    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill(color='black')
         pygame.display.flip()
-
+       #limits the framerate to 60 FPS
+        clk = clock.tick(60)
+        dt = clk / 1000
 
 if __name__ == "__main__":
     main()
